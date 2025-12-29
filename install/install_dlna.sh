8c6f4a281fedefa6a743edca67d20eb5b6c5206c
CarPlay Pi Maintainer <user@open-source-pi.org>
2025-12-29 11:07:17 -0600

    Fix package name: gmediarender

--- install/install_dlna.sh
+++ install/install_dlna.sh
@@ -4,6 +4,6 @@ set -e
 echo "Installing DLNA Renderer dependencies..."
 apt-get update
 # Install gmrender-resurrect (available in Bookworm) and GStreamer plugins
-apt-get install -y gmrender-resurrect gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-tools
+apt-get install -y gmediarender gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-tools
 
 echo "DLNA Renderer installed."
